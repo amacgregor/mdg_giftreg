aa2fa9c0a7a71f76a733d5e1ac37a2b41ec80acf
Allan MacGregor <dev@allanmacgregor.com>
2013-03-03 15:50:30 -0500

    Fixes to the block and layout files

--- app/code/local/Mdg/Giftregistry/Helper/Data.php
+++ app/code/local/Mdg/Giftregistry/Helper/Data.php
@@ -1,5 +1,9 @@
 <?php
 class Mdg_Giftregistry_Helper_Data extends Mage_Core_Helper_Abstract {
-
+    public function getEventTypes()
+    {
+        $collection = Mage::getModel('mdg_giftregistry/type')->getCollection();
+        return $collection;
+    }
 }
-?>
+
